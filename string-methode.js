b3e9afd59752c1186f1cd0f9ebebd3db401c753e
SohagHossain9983 <sohaghossain9983@gmail.com>
2025-06-25 14:04:59 +0600

    add strinh methode page

---
+++ string-methode.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+var sohag = 'I ma sohag';
+console.log(sohag)
+console.log(sohag.length)
+console.log(sohag.search('h'))
+console.log(sohag.charCodeAt(1))
+console.log(sohag.at(5))
+console.log(sohag.slice(6))
+
+
+
+
