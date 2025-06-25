7ed2fd99a8a0be89d35335b580d3bb54e1533eb9
SohagHossain9983 <sohaghossain9983@gmail.com>
2025-06-25 16:41:35 +0600

    add somthing

--- string-methode.js
+++ string-methode.js
@@ -3,14 +3,18 @@
 
 
 
-var sohag = 'I ma sohag';
-console.log(sohag)
-console.log(sohag.length)
-console.log(sohag.search('h'))
-console.log(sohag.charCodeAt(1))
-console.log(sohag.at(5))
-console.log(sohag.slice(6))
-
-
-
-
+var sohag = 'I am sohag';
+// console.log(sohag)
+// console.log(sohag.length)
+// console.log(sohag.search('h'))
+// console.log(sohag.charCodeAt(1))
+// console.log(sohag.at(5))
+// console.log(sohag.slice(6))
+// console.log(sohag.substring(4, 7))
+// console.log(sohag.substr(9, 6))
+// console.log(sohag.toUpperCase())
+// console.log(sohag.toLowerCase())
+console.log(sohag.replaceAll("o", "ho"))
+console.log(sohag.replaceAll("I", "My"))
+console.log(sohag.replaceAll("sohag", "riday"))
+console.log(sohag.concat(' hossain'))
