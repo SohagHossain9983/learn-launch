c3af7ed8f44d9e41daf487132a1e7b323adddb0a
SohagHossain9983 <sohaghossain9983@gmail.com>
2025-07-14 12:42:21 +0600

    nested loop practice

---
+++ loop.js
@@ -0,0 +1,137 @@
+
+
+
+
+// example 1
+// for (let number = 1; number < 10; number++) {
+//     console.log('count number :', number)
+// }
+// count number : 1
+// count number : 2
+// count number : 3
+// count number : 4
+// count number : 5
+// count number : 6
+// count number : 7
+// count number : 8
+// count number : 9
+
+
+// example 2
+// even number
+// for (let number = 1; number <=20; number++) {
+//     if (number % 2 == 0) {
+//         console.log('The even number is :', number)
+//     }
+// }
+// The even number is : 2
+// The even number is : 4
+// The even number is : 6
+// The even number is : 8
+// The even number is : 10
+// The even number is : 12
+// The even number is : 14
+// The even number is : 16
+// The even number is : 18
+// The even number is : 20
+
+
+
+// example 3
+// odd number
+// for (let number = 1; number < 20; number++) {
+//     if (number % 2 == 1) {
+//         console.log('The odd number is :', number)
+//     }
+// }
+// The odd number is : 1
+// The odd number is : 3
+// The odd number is : 5
+// The odd number is : 7
+// The odd number is : 9
+// The odd number is : 11
+// The odd number is : 13
+// The odd number is : 15
+// The odd number is : 17
+// The odd number is : 19
+
+
+// example 4
+// for (let number = 5; number <= 30; number= number + 5) {
+//     console.log(number)
+// }
+// 5
+// 10
+// 15
+// 20
+// 25
+// 30
+
+
+
+// // example 5
+// for (let number = 1; number <= 20; number= number + 3) {
+//     console.log('Distance number :', number)
+// }
+// Distance number : 1
+// Distance number : 4
+// Distance number : 7
+// Distance number : 10
+// Distance number : 13
+// Distance number : 16
+// Distance number : 19
+
+
+// example 6
+// for (let number = 5; number <= 40; number= number + 10) {
+//     console.log(' Distance nember:', number)
+// }
+//  Distance nember: 5
+//  Distance nember: 15
+//  Distance nember: 25
+//  Distance nember: 35
+
+
+
+
+example 7
+var newNumber = 10
+for (var i = 1; i <= newNumber; i++) {
+    var results = "";
+    for (var jk = 1; jk <= newNumber; jk++) {
+        results += " * ";
+    }
+    console.log(results)
+}
+//  *  *  *  *  *  *  *  *  *  * 
+//  *  *  *  *  *  *  *  *  *  * 
+//  *  *  *  *  *  *  *  *  *  *
+//  *  *  *  *  *  *  *  *  *  *
+//  *  *  *  *  *  *  *  *  *  *
+//  *  *  *  *  *  *  *  *  *  *
+//  *  *  *  *  *  *  *  *  *  *
+//  *  *  *  *  *  *  *  *  *  *
+//  *  *  *  *  *  *  *  *  *  *
+//  *  *  *  *  *  *  *  *  *  *
+
+
+
+// example 8
+// var newNumber2 = 10
+// for (var i = 1; i <= newNumber2; i++) {
+//     var results = "";
+//     for (var jkl = 1; jkl <= i; jkl++) {
+//         results += jkl + ' '
+//     }
+//     console.log(results)
+// }
+// 1 
+// 1 2 
+// 1 2 3
+// 1 2 3 4
+// 1 2 3 4 5
+// 1 2 3 4 5 6
+// 1 2 3 4 5 6 7
+// 1 2 3 4 5 6 7 8
+// 1 2 3 4 5 6 7 8 9
+// 1 2 3 4 5 6 7 8 9 10
---
+++ nested-loop.js
@@ -0,0 +1,92 @@
+
+
+// example 1
+// var newNumber2 = 10
+// for (var i = 1; i <= newNumber2; i++) {
+//     var results = "";
+//     for (var jkl = 1; jkl <= i; jkl++) {
+//         results += jkl + ' '
+//     }
+//     console.log(results)
+// }
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+// 1 2 3 4 5
+// 1 2 3 4 5 6
+// 1 2 3 4 5 6 7
+// 1 2 3 4 5 6 7 8
+// 1 2 3 4 5 6 7 8 9
+// 1 2 3 4 5 6 7 8 9 10
+
+
+// example 2
+// var newNumber = 10
+// for (var i = 1; i <= newNumber; i++) {
+//     var results = "";
+//     for (var jk = 1; jk <= newNumber; jk++) {
+//         results += " # ";
+//     }
+//     console.log(results)
+// }
+//  #  #  #  #  #  #  #  #  #  #
+//  #  #  #  #  #  #  #  #  #  #
+//  #  #  #  #  #  #  #  #  #  #
+//  #  #  #  #  #  #  #  #  #  #
+//  #  #  #  #  #  #  #  #  #  #
+//  #  #  #  #  #  #  #  #  #  #
+//  #  #  #  #  #  #  #  #  #  #
+//  #  #  #  #  #  #  #  #  #  #
+//  #  #  #  #  #  #  #  #  #  #
+//  #  #  #  #  #  #  #  #  #  #
+
+
+// example 3
+// for (let i = 5; i >= 1; i--) {
+//     let results = " ";
+//     for (let j = 1; j <= i; j++) {
+//         results +=  " # ";
+//     }
+//     console.log(results);
+// }
+//   #  #  #  #  # 
+//   #  #  #  # 
+//   #  #  #    
+//   #  #       
+//   #
+
+
+// example 4
+// for (let i = 5; i >= 1; i--) {
+//     let results = " ";
+//     for (let j = 1; j <= i; j++) {
+//         results += j + " ";
+//     }
+//     console.log(results);
+// }
+// 1 2 3 4 5
+// 1 2 3 4
+// 1 2 3
+// 1 2
+// 1
+
+
+// example 5
+// for (let i = 10; i >= 1; i--) {
+//     let results = "  ";
+//     for (let k = 1; k <= i; k++) {
+//         results += k + " "
+//     }
+//     console.log(results)
+// }
+//   1 2 3 4 5 6 7 8 9 10
+//   1 2 3 4 5 6 7 8 9
+//   1 2 3 4 5 6 7 8
+//   1 2 3 4 5 6 7
+//   1 2 3 4 5 6
+//   1 2 3 4 5
+//   1 2 3 4
+//   1 2 3
+//   1 2
+//   1
